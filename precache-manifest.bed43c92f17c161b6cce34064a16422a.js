c27fee35d3c9b78105d309530cd46392870c441a
Arjun Saini <arjunsaini2966@gmail.com>
2022-07-08 14:49:26 +0530

    Updates

---
+++ precache-manifest.bed43c92f17c161b6cce34064a16422a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "751fe472a9d0457f8f5c",
+    "url": "/github.io/vibe-notes/static/css/main.f34f0087.chunk.css"
+  },
+  {
+    "revision": "751fe472a9d0457f8f5c",
+    "url": "/github.io/vibe-notes/static/js/main.751fe472.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/github.io/vibe-notes/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "696ee17fb5899e85669e",
+    "url": "/github.io/vibe-notes/static/js/2.696ee17f.chunk.js"
+  },
+  {
+    "revision": "792101936015ac7dd40f",
+    "url": "/github.io/vibe-notes/static/js/runtime~main.79210193.js"
+  },
+  {
+    "revision": "2677012babc84078a6854343b1993a16",
+    "url": "/github.io/vibe-notes/index.html"
+  }
+];
